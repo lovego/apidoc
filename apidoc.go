2802c2f056e6009bb9a5ebff209502821008f7f6
luwei <wei.lu@hztl3.com>
2019-12-05 16:06:54 +0800

    必填项、拆分文档为单个文件。

--- apidoc.go
+++ apidoc.go
@@ -1,7 +1,6 @@
 package apidoc
 
 import (
-	"fmt"
 	"github.com/lovego/apidoc/router"
 	"io/ioutil"
 	"log"
@@ -23,67 +22,54 @@ type ResBodyTpl struct {
 
 var BaseRes = ResBodyTpl{Code: "ok", Message: "success"}
 
-type Doc struct {
-	Indexes  []string
-	Contents []string
+func GenDocs(r *router.R, workDir string) {
+	merge(r)
+	genDocs(r, ``, workDir)
 }
 
-func GenDocs(r *router.R, dirPath string) {
-	l1 := make([]string, 0)
+// genDocs Generate doc.
+// basePath is base router path.
+// dirPath is dictionary path.
+func genDocs(r *router.R, basePath, workDir string) {
+	basePath = basePath + r.Info.Path
+	currentWorkDir := workDir
+	if err := os.MkdirAll(workDir, 0755); err != nil {
+		log.Panic(err)
+	}
+	indexes := make([]string, 0)
 	for i := range r.Nodes {
-		o := r.Nodes[i]
-		workDir := path.Join(dirPath, o.Info.Path)
-		if err := os.MkdirAll(workDir, 0755); err != nil {
-			log.Panic(err)
+		child := r.Nodes[i]
+		title := child.Info.Title
+		if child.Info.IsEntry {
+			// 生成文档
+			fileName := title + `.md`
+			docStr := parseEntryDoc(child, basePath)
+			buf := []byte(docStr)
+			fullPath := filepath.Join(workDir, fileName)
+			if err := ioutil.WriteFile(fullPath, buf, 0666); err != nil {
+				log.Panic(err)
+			}
+			indexes = append(indexes, `[`+title+`](`+fullPath+`)`)
+		} else {
+			// group
+			if title != `` { // 有标题则认为需要创建新的子目录
+				workDir = path.Join(workDir, title)
+				indexes = append(indexes, `[`+title+`](`+workDir+`)`)
+			}
 		}
-		l1 = append(l1, fmt.Sprintf(`[%s](%s)`, getTitle(r), workDir))
-
-		l2 := make([]string, 0)
-		for _, obj := range o.Nodes {
-			NewDoc(obj, o.Info.Path).Create(workDir, obj.Info.Path)
-			l2 = append(l2, fmt.Sprintf(`[%s](%s)`, getTitle(r), path.Join(workDir, obj.Info.Path+`.md`)))
-		}
-
-		buf := []byte(strings.Join(l2, "\n"))
-		if err := ioutil.WriteFile(
-			filepath.Join(workDir, "README.md"), buf, 0666,
-		); err != nil {
-			log.Panic(err)
-		}
+		genDocs(child, basePath, workDir)
 	}
 
-	buf := []byte(strings.Join(l1, "\n"))
+	indexesBuf := []byte(strings.Join(indexes, `\n`))
 	if err := ioutil.WriteFile(
-		filepath.Join(dirPath, "README.md"), buf, 0666,
+		filepath.Join(currentWorkDir, `README.md`), indexesBuf, 0666,
 	); err != nil {
 		log.Panic(err)
 	}
 }
 
-func NewDoc(r *router.R, basePath string) *Doc {
-	d := &Doc{
-		Indexes:  make([]string, 0),
-		Contents: make([]string, 0),
-	}
-	d.Indexes = append(d.Indexes, fmt.Sprintf(`# %s <a name="index"></a>`, getTitle(r)))
-
-	for i := range r.Nodes {
-		d.Parse(r.Nodes[i], basePath, 1)
-	}
-	return d
-}
-
-func (d *Doc) Create(dir, name string) {
-	docs := append(append(d.Indexes, ``), d.Contents...)
-	buf := []byte(strings.Join(docs, "\n"))
-	if err := ioutil.WriteFile(
-		filepath.Join(dir, name+".md"), buf, 0666,
-	); err != nil {
-		log.Panic(err)
-	}
-}
+// merge same path group
 func merge(r *router.R) {
-	// merge same path group
 	if r == nil || r.Info.IsEntry {
 		return
 	}
@@ -114,44 +100,11 @@ func merge(r *router.R) {
 	r.Nodes = nodes
 }
 
-func (d *Doc) Parse(r *router.R, basePath string, level int) {
-	merge(r)
-	basePath += r.Info.Path
-	if !r.Info.IsEntry && r.Info.Path != `` && level < 3 {
-		anchorName := anchorNameReg.ReplaceAllStringFunc(basePath, func(s string) string {
-			res := `-`
-			return res
-		})
-
-		idx := strings.Repeat("\t", level-1) + `- `
-		idx += `[` + getTitle(r) + `](#` + anchorName + `)`
-		d.Indexes = append(d.Indexes, idx)
-		content := "\n" + strings.Repeat("#", level+1) + ` `
-		content += getTitle(r) + `<a name="` + anchorName + `"></a>`
-		d.Contents = append(d.Contents, content)
-		level += 1
-	}
-	if r.Info.IsEntry {
-		idx, c := parseRouterDoc(r, basePath, level)
-		d.Indexes = append(d.Indexes, idx)
-		d.Contents = append(d.Contents, c)
-	}
-	for i := range r.Nodes {
-		d.Parse(r.Nodes[i], basePath, level)
-	}
-}
-
-var anchorNameReg = regexp.MustCompile(`[^a-zA-Z0-9]+`)
-
-func parseRouterDoc(r *router.R, path string, level int) (idx, content string) {
+func parseEntryDoc(r *router.R, basePath string) (content string) {
+	urlPath := basePath + r.Info.Path
 	docs := make([]string, 0)
-	anchorName := r.Info.Method + anchorNameReg.ReplaceAllStringFunc(path, func(s string) string {
-		res := `-`
-		return res
-	})
-
 	// title
-	title := `#### ` + getTitle(r) + `<a name="` + anchorName + `"></a> [返回目录](#index)`
+	title := `# ` + r.Info.Title
 	docs = append(docs, title)
 
 	// description
@@ -160,26 +113,26 @@ func parseRouterDoc(r *router.R, path string, level int) (idx, content string) {
 	}
 
 	// URL
-	reqUrl := `##### ` + r.Info.Method + ` ` + path
+	reqUrl := `## ` + r.Info.Method + ` ` + urlPath
 	docs = append(docs, reqUrl)
 
 	// RegComments
 	if len(r.Info.RegComments) > 0 {
-		docs = append(docs, "\n"+`##### 正则参数说明`)
+		docs = append(docs, "\n"+`## 正则参数说明`)
 		for _, o := range r.Info.RegComments {
 			docs = append(docs, `- `+o.Field+`: `+o.Comment)
 		}
 	}
 	// QueryComments
 	if len(r.Info.QueryComments) > 0 {
-		docs = append(docs, "\n"+`##### Query 参数说明`)
+		docs = append(docs, "\n"+`## Query 参数说明`)
 		for _, o := range r.Info.QueryComments {
 			docs = append(docs, `- `+o.Field+`: `+o.Comment)
 		}
 	}
 	// req
 	if r.Info.Req != nil {
-		docs = append(docs, "\n"+`##### 请求体说明(`+r.Info.ReqContentType+`)`)
+		docs = append(docs, "\n"+`## 请求体说明(`+r.Info.ReqContentType+`)`)
 		docs = append(docs, "```json5")
 		docs = append(docs, parseJsonDoc(defaults.Set(r.Info.Req)))
 		docs = append(docs, "```")
@@ -189,31 +142,29 @@ func parseRouterDoc(r *router.R, path string, level int) (idx, content string) {
 	if r.Info.SucRes != nil {
 		res := BaseRes
 		res.Data = defaults.Set(r.Info.SucRes)
-		docs = append(docs, "\n"+`##### 返回体说明`)
+		docs = append(docs, "\n"+`## 返回体说明`)
 		docs = append(docs, "```json5")
 		docs = append(docs, parseJsonDoc(&res))
 		docs = append(docs, "```")
 	}
 	// error responses
 	if len(r.Info.ErrRes) > 0 {
-		docs = append(docs, "\n"+`##### 返回错误说明`)
+		docs = append(docs, "\n"+`## 返回错误说明`)
 		for i := range r.Info.ErrRes {
 			o := &r.Info.ErrRes[i]
 			o.Data = defaults.Set(o.Data)
-			docs = append(docs, "\n"+`- 错误码：`+o.Code)
+			docs = append(docs, "\n"+`### 错误码：`+o.Code)
 			docs = append(docs, "```json5")
 			docs = append(docs, parseJsonDoc(o))
 			docs = append(docs, "```")
 		}
 	}
-	// index
-	idx = strings.Repeat("\t", level-1) + `- `
-	idx += `[` + getTitle(r) + `](#` + anchorName + `)`
 	content = strings.Join(docs, "\n")
 	return
 }
 
 func parseJsonDoc(v interface{}) string {
+	const commentLineOffset = 50
 	data, err := json_doc.MarshalIndent(v, ``, `  `)
 	if err != nil {
 		log.Panic(err)
@@ -225,18 +176,17 @@ func parseJsonDoc(v interface{}) string {
 		res := r.FindAllStringSubmatch(list[i], -1)
 		if len(res) > 0 {
 			str := r.ReplaceAllString(list[i], `":`)
-			str += ` // ` + res[0][1]
+			comment := strings.TrimSpace(res[0][1])
+			if comment[0] == '*' {
+				comment = `【必需】` + comment
+			}
+			repeatTimes := commentLineOffset - len(str)
+			if repeatTimes < 1 {
+				repeatTimes = 1
+			}
+			str += strings.Repeat(` `, repeatTimes) + `// ` + comment
 			list[i] = str
 		}
 	}
 	return strings.Join(list, "\n")
-}
-
-// getTitle return router title, if is empty return "无标题"
-func getTitle(r *router.R) string {
-	t := r.Info.Title
-	if t == `` {
-		t = `无标题`
-	}
-	return t
 }
--- apidoc_test.go
+++ apidoc_test.go
@@ -11,36 +11,34 @@ type req struct {
 }
 
 type res struct {
-	Name string `json:"name" c:"名称"`
-	Age  string `json:"age" c:"年龄"`
-	Res  string `json:"res" c:"返回信息"`
+	Name string `json:"name" doc:"名称"`
+	Age  string `json:"age" doc:"年龄"`
+	Res  string `json:"res" doc:"返回信息"`
 }
 
 type errorRes struct {
-	Name   string `json:"name" c:"名称"`
-	Age    string `json:"age" c:"年龄"`
+	Name   string `json:"name" c:"*名称"`
+	Age    string `json:"age" c:"*年龄"`
 	ErrRes string `json:"errRes" c:"错误返回信息"`
 }
 
 func ExampleGenDocs() {
 
 	goaRouter := goa.New()
-	r0 := router.New(&goaRouter.RouterGroup, ``)
-	r := r0.Group(`/purchases`).Title(`采购`)
-	g1 := r.Group(`/order`).Title(`订单`)
-	g2 := r.Group(`/arrival`).Title(`到货单`)
+	rootRouter := router.New(&goaRouter.RouterGroup, ``)
+	firstLevel := rootRouter.Group(`/purchases`).Title(`采购`)
+	secondLevel := firstLevel.Group(`/arrival`).Title(`到货单`)
 
-	g1.GetX(`/users`, func(c *goa.Context) {}).Doc(`用户`, ``, ``, nil, nil)
-	g2.Post(`/book`, func(c *goa.Context) {}).Title(`订餐`).
+	secondLevel.Post(`/book`, func(c *goa.Context) {}).Title(`订餐`).
 		Desc(`描述信息描述信息描述信息描述信息描述信息描述信息描述信息`).
+		Regex(`id:公司ID`).
+		Query(`qid:公司QID;qid2:公司`).
 		Req(&req{}).
 		Res(&res{}).
 		AddErrRes(`something-wrong`, `some thing wrong`, &errorRes{}).
 		AddErrRes(`something-wrong2`, `some thing wrong2`, &errorRes{})
 
-	g1.Group(`/child`).Title(`1111`)
-
-	GenDocs(r0, `docs`)
+	GenDocs(rootRouter, `docs`)
 
 	// Output:
 }
